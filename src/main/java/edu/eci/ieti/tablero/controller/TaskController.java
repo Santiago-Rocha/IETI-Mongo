cc32993b3277ba863c57711028e79ad795226574
Santiago-Rocha <santiago.rocha@tulpep.com>
2019-10-16 15:17:07 -0500

    CrossOrigin

--- src/main/java/edu/eci/ieti/tablero/controller/TaskController.java
+++ src/main/java/edu/eci/ieti/tablero/controller/TaskController.java
@@ -3,6 +3,7 @@ package edu.eci.ieti.tablero.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import edu.eci.ieti.tablero.model.Task;
 import edu.eci.ieti.tablero.model.User;
 import edu.eci.ieti.tablero.services.ITaskService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class TaskController {
--- src/main/java/edu/eci/ieti/tablero/controller/UserController.java
+++ src/main/java/edu/eci/ieti/tablero/controller/UserController.java
@@ -10,6 +10,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.eci.ieti.tablero.model.User;
 import edu.eci.ieti.tablero.services.IUserService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UserController {
 
